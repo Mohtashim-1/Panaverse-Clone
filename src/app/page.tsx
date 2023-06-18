6c3e9679e7b16f60fd70ee67ee1ef9a4678c1f7b
Mohtashim-1 <64360314+Mohtashim-1@users.noreply.github.com>
2023-06-18 14:54:59 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         {/* Specialized Track */}
         <SpecializedTrack />
         {/* Quarter Box */}
-        <QuarterBox />
+        {/* <QuarterBox /> */}
       </main>
     </>
   );
